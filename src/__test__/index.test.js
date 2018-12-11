0aa5324e2d9d253ee4a5309951a362a70877746c
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-12-11 11:46:03 +0100

    Add jest and tests

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -1,3 +1,11 @@
 it("1 + 1 equals 2", async () => {
   expect(1 + 1).toEqual(2);
 });
+
+it("2 + 1 equals 3", async () => {
+  expect(2 + 1).toEqual(3);
+});
+
+it("sum associative", async () => {
+  expect(2 + 1 + 3).toEqual(2 + (1 + 3));
+});
